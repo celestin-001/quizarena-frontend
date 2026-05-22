fa52b1b0f0b868e121cc760e286604f2ae7bdad0
celestin-001 <goumoucelestin3@gmail.com>
2026-05-22 08:32:37 +0200

    add App index main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css'        // styles globaux
+import App from './App.tsx' // ton application
+import './i18n';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
